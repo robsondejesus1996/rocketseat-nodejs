0c7a17378e8b15455f2ed96457fba020576de0a5
Robson de Jesus <robsondejesus1996@hotmail.com>
2023-12-25 17:18:52 -0300

    Criação de rodas e teste da api no insomnia

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,8 +1,27 @@
 import http from 'node:http'
 
+// GET => BUSCA INFORMAÇÃO
+// POST => CRIAR ALGUMA COISA
+// PUT => EDITAR OU ATUALIZAR UM RECURSO(ATUALIZAR MUITAS COISAS AO MESMO TEMPO)
+// PATCH => ATUALIZAR UMA INFORMAÇÃO ÚNICA OU ESPECÍFICA NO BACK-END 
+// DELETE => DELETAR UM RECURSO DO BACK-END
+
 //requisição e responsta
 const server = http.createServer((req, res) =>{
-    return res.end('Olá mundo')
+
+    const { method, url } = req
+
+    if(method === 'GET' && url === '/users'){
+        return res.end('Listagem de users')
+    }
+
+    if(method === 'POST' && url === '/users'){
+        return res.end('Criação de Usuários')
+    }
+
+
+    return res.end('Hello World')
+    
 })
 
 
